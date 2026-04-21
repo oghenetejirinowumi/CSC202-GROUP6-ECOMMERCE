7bb0bdc5e7433da71bdb088543a29a1002790c70
oghenetejirinowumi <oghenetejirin.owumi@pau.edu.ng>
2026-04-21 13:16:07 +0100

    FULL PROJECT SETUP (backend + frontend)

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-white min-h-screen">
+      {/* This page is intentionally left blank to only show the Navbar */}
+    </main>
+  );
+}
