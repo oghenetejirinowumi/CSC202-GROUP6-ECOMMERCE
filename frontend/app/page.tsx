07631380b2458d4269a1b031f3954fc2330e6cb0
Deomen20 <chidera.omenuko@pau.edu.ng>
2026-05-19 19:39:37 +0100

    feat: created the login and signup pages and Refactor code structure for improved readability and maintainability

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,16 @@
 import Hero from "../components/Hero";
+import LoginPage from "./login/page";
+import SignupPage from "./signup/page";
 // import FeaturedProducts from "../components/FeaturedProducts";
 export default function Home() {
   return (
     <>
-      <Hero />
+    {/* <SignupPage/> */}
+    <LoginPage/>
+      {/* <Hero /> */}
       {/* <FeaturedProducts/> */}
       </>
   );
+
+  
 }
